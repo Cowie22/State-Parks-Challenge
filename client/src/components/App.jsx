9d7d77d77ec014839e35a4976c6227492a178f00
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-06-05 21:42:46 -0700

    Sets up api and recieving data on request

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import Axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      parkData: [],
     };
+  }
+  componentDidMount() {
+    this.getData();
+  }
+  getData() {
+    Axios({
+      method: 'get',
+      url: 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=5tzMjp0vTJgSS7TSY2M2oRu2Par3WdpqYcNQEKt4',
+    })
+      .then(res => {
+        console.log(res)
+        this.setState({
+          parkData: res.data,
+        })
+      })
+      console.log(this.state.parkData)
   }
   render() {
     return (
